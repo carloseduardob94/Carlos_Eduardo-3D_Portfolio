bcb38846f1c5ac276781b2511d820a86c7d56790
Carlos Eduardo <educonts08@gmail.com>
2025-06-23 20:55:25 -0300

    feat: new component created - LogoSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import LogoSection from "./components/LogoSection";
 import NavBar from "./components/NavBar";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
@@ -10,6 +11,7 @@ const App = () => {
       </div>
       <Hero />
       <ShowcaseSection />
+      <LogoSection />
     </>
   )
 }
---
+++ src/components/LogoSection.tsx
@@ -0,0 +1,45 @@
+import { logoIconsList } from "../constants";
+
+interface LogoIconProps {
+  icon: {
+    imgPath: string;
+    name: string;
+  }
+}
+
+const LogoIcon = ({ icon }: LogoIconProps) => {
+  return (
+    <div className="flex-none flex-center marquee-item">
+      <img src={icon.imgPath} alt={icon.name} />
+    </div>
+  )
+}
+
+const LogoSection = () => {
+  return (
+    <div className="md:my-20 my-10 relative">
+      <div className="gradient-edge"></div>
+      <div className="gradient-edge"></div>
+
+      <div className="marquee h-52">
+        <div className="marquee-box md:gap-12 gap-5">
+          {logoIconsList.map((icon: any) => (
+            <LogoIcon
+              key={icon.name}
+              icon={icon}
+            />
+          ))}
+
+          {logoIconsList.map((icon: any) => (
+            <LogoIcon
+              key={icon.name}
+              icon={icon}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LogoSection;
